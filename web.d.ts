1cecc3e77d996ff59f145692161b448ab7d0c1ce
zerkalica <zerkalica@gmail.com>
2024-12-08 09:30:20 +0000

    Deploying to gh-pages from @ zerkalica/yuf@9e9adf8575ef630ebad27c15bb2b74b32ef5eaea 🚀

---
+++ web.d.ts
@@ -0,0 +1,17 @@
+declare let _$_: {
+    new (): {};
+} & typeof globalThis;
+declare class $ extends _$_ {
+}
+declare namespace $ {
+    export type $ = typeof $$;
+    export class $$ extends $ {
+    }
+    namespace $$ {
+        type $$ = $;
+    }
+    export {};
+}
+
+export = $;
+//# sourceMappingURL=web.d.ts.map
