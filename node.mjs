d37293b1b9f59b52f3d850b16170a1009048c237
zerkalica <zerkalica@users.noreply.github.com>
2024-12-18 07:16:03 +0000

    Deploying to gh-pages from @ zerkalica/yuf@7d0abd659fb62f222ad754e7b47614c275b45c46 🚀

--- node.mjs
+++ node.mjs
@@ -4750,7 +4750,8 @@ var $;
                     stabilityThreshold: 100,
                 },
             });
-            watcher.on('all', (type, path) => {
+            watcher
+                .on('all', (type, path) => {
                 if (path instanceof Error) {
                     this.$.$mol_fail_log(path);
                     return;
@@ -4763,7 +4764,8 @@ var $;
                 else {
                     file.parent().reset();
                 }
-            });
+            })
+                .on('error', $mol_fail_log);
             return {
                 destructor() {
                     watcher.close();
