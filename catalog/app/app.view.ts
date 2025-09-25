67d694c2b2a1db92f4756f1effcfe7b8ac8aef78
Stefan Zerkalica <zerkalica@gmail.com>
2025-09-26 01:48:10 +0300

    Added non-auth slot links in app menu

--- catalog/app/app.view.ts
+++ catalog/app/app.view.ts
@@ -1,31 +1,35 @@
 namespace $.$$ {
 	export class $yuf_catalog_app extends $.$yuf_catalog_app {
+
 		@$mol_mem
-		override pages(): readonly $mol_view[] {
-			const prev  = $mol_wire_probe(() => this.pages())
-			try {
-				const logged = this.logged()
-				if ( logged ) return super.pages()
-				return [ this.Login() ]
-			} catch (e) {
-				if ($mol_promise_like(e)) return prev ?? []
-				$mol_fail_hidden(e)
-			}
+		override pages() {
+			const logged = this.logged()
+			if ( logged ) return super.pages()
+			return [ this.Login() ]
+		}
+
+		override menu_links_authorized() {
+			return this.logged() ? super.menu_links_authorized() : []
+		}
+
+		override menu_links_no_authorized() {
+			return this.logged() ? [] : super.menu_links_no_authorized()
 		}
 
 		override logout_click(e?: Event) {
-			e && $mol_dom_event.wrap(e).prevented(true)
 			this.logout()
 
 			this.$.$mol_wait_timeout(100)
 
-			const close_arg = this.spread_close_arg()
 			const arg = this.$.$mol_state_arg
-			const link = arg.link(close_arg)
+
+			const link = arg.link(this.spread_close_arg())
 			arg.href(link)
 		}
 
-		override placeholders() { return this.spread() || ! this.logged() ? [] : super.placeholders() }
+		override placeholders() {
+			return this.spread() || ! this.logged() ? [] : super.placeholders()
+		}
 
 	}
 }
