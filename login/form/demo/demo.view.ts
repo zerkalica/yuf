6d7a87d0ff9eb479e5ae44386d2bda8d379d5e94
Stefan Zerkalica <zerkalica@gmail.com>
2025-09-30 13:44:24 +0300

    Increased demo login form save delay

--- login/form/demo/demo.view.ts
+++ login/form/demo/demo.view.ts
@@ -2,7 +2,7 @@ namespace $.$$ {
 	export class $yuf_login_form_demo extends $.$yuf_login_form_demo {
 		
 		override enter(e?: Event) {
-			this.$.$mol_wait_timeout(1000)
+			this.$.$mol_wait_timeout(5000)
 			console.log('Logged:', this.login(), this.password())
 		}
 
