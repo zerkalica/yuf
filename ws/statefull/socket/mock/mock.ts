1fd740b28d505bfe01faed41d8a0b49ad4e1a8a5
Stefan Zerkalica <zerkalica@gmail.com>
2025-10-06 00:21:42 +0300

    Adds $yuf_ws_statefull_socket_mock

---
+++ ws/statefull/socket/mock/mock.ts
@@ -0,0 +1,36 @@
+namespace $ {
+	export class $yuf_ws_statefull_socket_mock extends $yuf_ws_socket_mock<$yuf_ws_statefull_message> {
+		message_sub(obj: $yuf_ws_statefull_message) {
+			return this.$.$yuf_entity2.active_model(this.message_normalize(obj))
+		}
+
+		override answer(obj: $yuf_ws_statefull_message, periodically = false) {
+			if (obj.type === 'ping') return { type: 'pong' }
+
+			if (obj.error === null) return null // unsubscribe
+			try {
+
+				const first = this.message_sub(obj)
+				if (! first) return null
+
+				if (periodically && ! first.mock_periodically() ) return null
+				const data = first.mock(obj.data as {}) ?? null
+				return { ... obj, data }
+			} catch (e) {
+				if (e instanceof Error) {
+					const code = (e instanceof $yuf_transport_error ? e.cause.code : undefined) ?? 'INTERNAL_ERROR'
+					return {
+						... obj,
+						error: code,
+						message: e.message ?? ''
+					}
+				}
+				$mol_fail_hidden(e)
+			}
+		}
+
+		override message_normalize({ type, query, device, id }: Partial<$yuf_ws_statefull_message>) {
+			return { type, id, query, device }
+		}
+	}
+}
