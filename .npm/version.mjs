6521dae001c111a1ca309795941bedd4f20d94f2
Stefan Zerkalica <zerkalica@gmail.com>
2025-10-21 12:00:53 +0300

    Version script added

---
+++ .npm/version.mjs
@@ -0,0 +1,34 @@
+// @ts-check
+import { execSync } from 'node:child_process'
+import { readFileSync } from 'node:fs'
+import { join } from 'node:path'
+
+const cwd = typeof Deno !== 'undefined' ? Deno.cwd.bind(Deno) : process.cwd.bind(process)
+
+let version = ''
+
+try {
+  version = execSync('git describe --long --tags').toString()
+  version = version.replace(/\-0\-.*/, '').replace(/\-[\d]+\-/, '-')
+} catch (e) {}
+
+let hash = ''
+
+if (! version) {
+  try {
+    hash = execSync('git rev-parse --short HEAD').toString()
+  } catch (e) {}
+
+  let raw = ''
+
+  try {
+    raw = readFileSync(join(cwd(), 'version.ts')).toString()
+  } catch (e) {}
+
+  version = raw.match(/["']([\d\.]+(?:\-[\d\w]+)?)["']/)?.[1] ?? ''
+}
+
+if (! version ) version = '0.0.1'
+if (hash) version = version.replace(/\-[\w\d]+$/, '') + `-${hash}`
+
+console.log(version)
