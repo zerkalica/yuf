759421b14b48da06e26846cd7770d4004b2e492d
Stefan Zerkalica <zerkalica@gmail.com>
2025-10-31 19:28:30 +0300

    Constant ids in catalog demo

--- catalog/demo/user/store.ts
+++ catalog/demo/user/store.ts
@@ -2,9 +2,12 @@ namespace $ {
 	export class $yuf_catalog_demo_user_store extends $mol_object {
 		@ $mol_mem
 		ids_all() {
+			const prefix = this.friend_user_id()
 			const ids = [] as string[]
 			for (let i = 0; i < 20; i++) {
-				ids.push($mol_guid())
+				let id = '' + (i + 1)
+				if (prefix) id = prefix + '-' + id
+				ids.push(id)
 			}
 
 			return ids
