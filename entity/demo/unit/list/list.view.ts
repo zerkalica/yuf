bb28ea0ff646b0143ad13c7131cc6705bde9429e
Stefan Zerkalica <zerkalica@gmail.com>
2025-12-04 12:40:46 +0300

    Removed unused deprecated $yuf_entity

--- entity/demo/unit/list/list.view.ts
+++
@@ -1,7 +0,0 @@
-namespace $.$$ {
-	export class $yuf_entity_demo_unit_list extends $.$yuf_entity_demo_unit_list {
-		override factory() {
-			return this.$.$yuf_entity_test_unit
-		}
-	}
-}
--- entity/page/page.view.ts
+++
@@ -1,4 +0,0 @@
-namespace $.$$ {
-	export class $yuf_entity_page extends $.$yuf_entity_page {
-	}
-}
