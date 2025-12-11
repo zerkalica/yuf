1b8215009a66abd77f1c1fdff56f9137e9ba2639
Stefan Zerkalica <zerkalica@gmail.com>
2025-12-11 21:38:12 +0300

    fix types

--- localizer/settings/form/form.view.ts
+++ localizer/settings/form/form.view.ts
@@ -1,9 +1,5 @@
 namespace $.$$ {
 	export class $yuf_localizer_settings_form extends $.$yuf_localizer_settings_form {
 		
-		override available_langs() {
-			return this.langs_str().split(',').map(str => str.trim()).filter(Boolean)
-		}
-		
 	}
 }
