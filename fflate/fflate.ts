4518425526056f6b7ddb0c0c000dd121b9b3ba10
Stefan Zerkalica <zerkalica@gmail.com>
2026-03-27 23:21:35 +0300

    fflate improved

--- fflate/fflate.ts
+++ fflate/fflate.ts
@@ -15,5 +15,40 @@ namespace $ {
 			return this.module().zipSync(...args)
 		}
 
+		protected fflate() { return this.$.$mol_static.$yuf_fflate }
+
+		data() { return new Uint8Array() }
+
+		@ $mol_mem
+		protected files() {
+			$mol_wire_solid()
+
+			const bin = this.data()
+			const files = this.fflate().unzip(bin)
+
+			return files
+		}
+
+		@ $mol_mem
+		names() { return Object.keys(this.files()) }
+
+		@ $mol_mem_key
+		file(name: string) {
+			$mol_wire_solid()
+			const files = this.files()
+
+			const data = files[name]
+			if (! data) throw new Error('File not found in zip', { cause: { name } })
+
+			return data
+		}
+
+		@ $mol_mem_key
+		text(name: string) {
+			$mol_wire_solid()
+			const template_buf = this.file(name)
+
+			return this.$.$mol_charset_decode(template_buf)
+		}
 	}
 }
