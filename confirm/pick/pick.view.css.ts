ad754e6b2e20a8ad571deff1d67152f31c7955b0
Stefan Zerkalica <zerkalica@gmail.com>
2026-04-15 18:36:19 +0300

    confirm dialog

---
+++ confirm/pick/pick.view.css.ts
@@ -0,0 +1,19 @@
+namespace $.$$ {
+	
+	$mol_style_define( $yuf_confirm_pick, {
+		
+		Content: {
+			padding: $mol_gap.block,
+			flex: {
+				direction: 'column',
+			},
+			gap: $mol_gap.block,
+		},
+		Buttons: {
+			justifyContent: 'center',
+			gap: $mol_gap.block,
+		},
+		
+	} )
+	
+}
---
+++ confirm/pick/pick.view.ts
@@ -0,0 +1,9 @@
+namespace $.$$ {
+	export class $yuf_confirm_pick extends $.$yuf_confirm_pick {
+		
+		override no_click() {
+			this.showed(false)
+		}
+		
+	}
+}
