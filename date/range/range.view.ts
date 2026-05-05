1a09539399b7efb3bc62290bcdedd1b4d7db3586
Stefan Zerkalica <zerkalica@gmail.com>
2026-05-05 23:35:48 +0300

    Date range fix

---
+++ date/range/range.view.ts
@@ -0,0 +1,9 @@
+namespace $.$$ {
+	export class $yuf_date_range_date extends $.$yuf_date_range_date {
+		
+		override value_moment(next?: $mol_time_moment | null) {
+			return this.moment(next) as $mol_time_moment
+		}
+		
+	}
+}
