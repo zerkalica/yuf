63c71a546ce3546db46327e9294e6e1cadfd486e
zerkalica <zerkalica@gmail.com>
2026-07-17 18:11:46 +0000

    Deploying to gh-pages from @ zerkalica/yuf@d39eabaae612d7cdea3168b2a7db3d9cd52163ed 🚀

--- web.js
+++ web.js
@@ -28193,7 +28193,7 @@ var $;
         class $yuf_number extends $.$yuf_number {
             blur_event() {
                 const next = this.value(this.value_string() ? undefined : this.value_min());
-                this.value_string(String(next || '0'));
+                this.value_string(String(next || ''));
             }
         }
         $$.$yuf_number = $yuf_number;
