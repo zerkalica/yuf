66a21ad29de3e42a7da58bccb841924155e318b4
Stefan Zerkalica <zerkalica@gmail.com>
2026-09-21 23:56:41 +0300

    Fix time duration

--- time/duration/duration.view.ts
+++ time/duration/duration.view.ts
@@ -1,6 +1,6 @@
 namespace $.$$ {
 
-	function declOfNum(number: number, titles: readonly string[]) {
+	function decl_ru(number: number, titles: readonly string[]) {
 		const cases = [2, 0, 1, 1, 1, 2]
 		number = Math.abs(number)
 		return titles[
@@ -8,6 +8,19 @@ namespace $.$$ {
 				? 2
 				: cases[ (number % 10 < 5) ? number % 10 : 5 ]
 		]
+	}
+
+	function decl_common(number: number, titles: readonly string[]) {
+		return number === 1 ? titles[0] : titles[1]
+	}
+
+	function decl_ar(number: number, titles: readonly string[]) {
+		return number === 1 ? titles[0] : number === 2 ? titles[1] : titles[2]
+	}
+
+	const rules = {
+		ru: decl_ru,
+		ar: decl_ar
 	}
 
 	export class $yuf_time_duration extends $.$yuf_time_duration {
@@ -19,7 +32,10 @@ namespace $.$$ {
 			const data = this.normalized()
 			type time_parts = 'year' | 'month' | 'day' | 'hour' | 'minute' | 'second'
 			let value = key in data ? Math.round(data[key as time_parts]) : null
-			const text = value ? declOfNum(value, this[key as time_parts]().split('|')) : ''
+			const lang = this.$.$mol_locale.lang()
+			const text = value
+				? (rules[lang as keyof typeof rules] || decl_common)( value, this[key as time_parts]().split('|') )
+				: ''
 
 			const normalized = ! value ? null : this.show_minus() ? value : Math.abs(value)
 
