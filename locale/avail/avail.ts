13685f097a4c4aa6d98ff6be7694c8d34de68bc4
Stefan Zerkalica <zerkalica@gmail.com>
2026-09-23 21:56:02 +0300

    Removed avail

--- locale/avail/avail.ts
+++
@@ -1,6 +0,0 @@
-namespace $ {
-	export const $yuf_locale_avail: Record<string, string> = {
-		en: 'EN',
-		ru: 'RU',
-	}
-}
--- locale/switch/switch.view.ts
+++ locale/switch/switch.view.ts
@@ -1,6 +1,5 @@
 namespace $.$$ {
 	export class $yuf_locale_switch extends $.$yuf_locale_switch {
-		override langs_available() { return this.$.$yuf_locale_avail }
 		override lang(next?: string) { return this.$.$mol_locale.lang(next) }
 	}
 }
