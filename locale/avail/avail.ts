45fbef7d1ef77abada49bd590b0b67725e66ff4e
Stefan Zerkalica <zerkalica@gmail.com>
2026-09-23 21:42:49 +0300

    locale switcher refactor

---
+++ locale/avail/avail.ts
@@ -0,0 +1,6 @@
+namespace $ {
+	export const $yuf_locale_avail: Record<string, string> = {
+		en: 'EN',
+		ru: 'RU',
+	}
+}
---
+++ locale/switch/switch.view.ts
@@ -0,0 +1,6 @@
+namespace $.$$ {
+	export class $yuf_locale_switch extends $.$yuf_locale_switch {
+		override langs_available() { return this.$.$yuf_locale_avail }
+		override lang(next?: string) { return this.$.$mol_locale.lang(next) }
+	}
+}
