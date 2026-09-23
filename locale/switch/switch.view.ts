bcf464d67a06e2ecd6b3b64a5287181ee747b938
Stefan Zerkalica <zerkalica@gmail.com>
2026-09-23 22:25:44 +0300

    computed dictionary from langs

--- locale/switch/switch.view.ts
+++ locale/switch/switch.view.ts
@@ -1,5 +1,9 @@
 namespace $.$$ {
 	export class $yuf_locale_switch extends $.$yuf_locale_switch {
 		override lang(next?: string) { return this.$.$mol_locale.lang(next) }
+		@ $mol_mem
+		override dictionary(next?: Record<string, string>) {
+			return next ?? Object.fromEntries(this.langs().map(code => [ code, code ]))
+		}
 	}
 }
